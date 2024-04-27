a68991a11d71b21a650dce4d7caaf14085817dea
khwong-c <kin.hin.wong.c@gmail.com>
2024-04-27 13:31:47 +0100

    Commit2

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,55 @@
-import {Editor, EditorProps} from '@atlaskit/editor-core';
-import { useSharedPluginState } from '@atlaskit/editor-common/hooks';
-import type {EditorView} from '@atlaskit/editor-prosemirror/view';
+import { useCallback, useState } from 'react';
 
-import {token} from '@atlaskit/tokens';
-import {CSSProperties, useEffect} from "react";
-import {useUniversalPreset} from "@atlaskit/editor-core/preset-universal";
-import {usePreset} from "@atlaskit/editor-core/use-preset";
+import { jsx } from '@emotion/react';
 
-import createSelectedPlugin from "./selectedPlugin.tsx";
-import {createDefaultPreset} from "@atlaskit/editor-core/preset-default";
+import { IconButton } from '@atlaskit/button/new';
+import MoreIcon from '@atlaskit/icon/glyph/more';
+import { ButtonItem, LinkItem, PopupMenuGroup, Section } from '@atlaskit/menu';
+import Popup from '@atlaskit/popup';
+import {
+    Header,
+    NavigationHeader,
+    NestableNavigationContent,
+    SideNavigation,
+} from '@atlaskit/side-navigation';
 
-const wrapperCss: CSSProperties = {
-    height: '100vh',
-}
-const contentCss: CSSProperties = {
-    padding: `0 ${token('space.250', '20px')}`,
-    height: '100%',
-    background: token('color.background.neutral.subtle', '#fff'),
-    boxSizing: 'border-box',
-}
+import {
+    Content,
+    LeftSidebar,
+    Main,
+    PageLayout,
+    RightSidebar,
+    useLeftSidebarFlyoutLock,
+} from '@atlaskit/page-layout';
 
-
-export default function App() {
-    const editorProps: EditorProps = {
-        appearance: "full-page",
-        allowUndoRedoButtons: true,
-        allowRule: true,
-        allowTables: true,
-        allowBorderMark: true,
-
-        allowExpand: true,
-
-        allowTasksAndDecisions: true,
-        allowBreakout: true,
-        allowHelpDialog: true,
-        allowPanel: true,
-        allowTemplatePlaceholders: true,
-        allowDate: true,
-        allowLayouts: true,
-        allowStatus: true,
-        allowTextAlignment: true,
-        allowIndentation: true,
-        showIndentationButtons: true,
-
-        allowFragmentMark: true,
-        autoScrollIntoView: true,
-
-        allowTextColor: true,
-    }
-    const universalPreset = useUniversalPreset({props: editorProps})
-    const {preset, editorApi} = usePreset(() => {
-        return universalPreset.add(createSelectedPlugin);
-    }, []);
-    const {selectedPluginState} = useSharedPluginState(editorApi, ['selectedPlugin']);
+import KitchenEditor from './KitchenEditor.tsx'
 
 
-    return <div style={wrapperCss}>
-        <div style={contentCss}>
-            <p>{JSON.stringify(selectedPluginState)}</p>
-            <Editor
-                preset={preset}
-                // onChange={handleChangeInTheEditor}
-                // onEditorReady={onEditorReady}
-                // editorActions={}
-            />
-        </div>
-    </div>
+export default function App(){
+    return (
+        <PageLayout>
+            <Content>
+                <LeftSidebar width={450}>
+                    <SideNavigation label="Project navigation">
+                        <NavigationHeader>
+                            <Header description="Sidebar header description">
+                                Sidebar Header
+                            </Header>
+                        </NavigationHeader>
+                        <NestableNavigationContent initialStack={[]}>
+                            <Section>
+                                <LinkItem href="http://www.atlassian.com">
+                                    Atlassian
+                                </LinkItem>
+                            </Section>
+                        </NestableNavigationContent>
+                    </SideNavigation>
+                </LeftSidebar>
+                <Main>
+                    <h1>Main Content</h1>
+                    <KitchenEditor/>
+                </Main>
+            </Content>
+        </PageLayout>
+    )
 }
